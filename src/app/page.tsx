b2f2b450f6c2d7fb6276aabc2011fe65cb9aa6a4
sparq-corp <96791338+sparq-corp@users.noreply.github.com>
2024-03-12 11:57:57 +0530

    added address changed font color and linkedIn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import { IoLogoInstagram } from 'react-icons/io';
 import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa6';
 import { MdEmail } from 'react-icons/md';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center bg-black sm:flex-row">
+    <main className="flex flex-col items-center justify-center sm:flex-row">
       <div className="w-[50%] flex justify-center items-center">
         <Image
           className="md:w-[300px] md:h-[300px] lg:w-[500px] lg:h-[500px]"
@@ -16,25 +17,25 @@ export default function Home() {
           alt="Growxth_logo"
         />
       </div>
-      <div className="py-4 bg-black sm:py-16 lg:py-12 md:w-[50%] bg-[#FFEAE6]">
+      <div className="py-4 sm:py-16 lg:py-12 md:w-[50%]">
         <div className="container px-4 text-left lg:px-4 lg:pr-28">
           <div className="space-y-1 lg:max-w-[450px] md:max-w-[350px]">
-            <h3 className="text-sm tracking-wide text-white md:text-base lg:text-xl/relaxed dark:text-white">
+            <h3 className="text-sm tracking-wide text-black md:text-base lg:text-xl/relaxed dark:text-white">
               Get ready for
             </h3>
-            <h1 className="text-4xl font-bold tracking-tighter text-white sm:text-5xl md:text-4xl lg:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tighter text-black sm:text-5xl md:text-4xl lg:text-6xl">
               something thrilling!
             </h1>
-            <h2 className="pt-2 text-sm tracking-tight text-white sm:text-xl md:text-lg lg:text-xl ">
+            <h2 className="pt-2 text-sm tracking-tight text-black sm:text-xl md:text-lg lg:text-xl ">
               We&apos;re Sri Lanka&apos;s 1st Revenue-Based Marketing Firm, once
               known as IDES, but now sporting a snazzy new identity - GROWXTH!
             </h2>
-            <h2 className="pt-2 text-sm tracking-tight text-white sm:text-xl md:text-lg lg:text-xl ">
+            <h2 className="pt-2 text-sm tracking-tight text-black sm:text-xl md:text-lg lg:text-xl ">
               While under our old alias IDES, we are going to be the masterminds
               behind launching fresh brands in the realms of tech, real estate,
               e-commerce, and finance & investments.
             </h2>
-            <h2 className="pt-2 text-sm tracking-tight text-white sm:text-xl md:text-lg lg:text-xl ">
+            <h2 className="pt-2 text-sm tracking-tight text-black sm:text-xl md:text-lg lg:text-xl ">
               This calls for more web launches and a heap of elbow grease. Keep
               that excitement alive!
             </h2>
@@ -48,20 +49,30 @@ export default function Home() {
                 target="_blank"
                 className="flex items-center justify-center"
               >
-                <IoLogoInstagram size={25} color="white" />
+                <IoLogoInstagram size={25} color="black" />
               </Link>
               <Link
-                href="https://www.facebook.com/profile.php?id=61557357473453&mibextid=ZbWKwL"
+                href="https://www.linkedin.com/company/growxth"
                 target="_blank"
                 className="flex items-center justify-center ml-2"
               >
-                <FaFacebookF size={20} color="white" />
+                <FaFacebookF size={20} color="black" />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/company/growxth"
+                target="_blank"
+                className="flex items-center justify-center ml-2"
+              >
+                <FaLinkedin size={22} color="black" />
               </Link>
               {/* <div className="flex items-center justify-center ml-2 target='_blank'">
                 <MdEmail size={20} color='white' />
               </div> */}
-              <p className="flex ml-6 text-white ju">www.growxth.com</p>
+              <p className="flex ml-6 text-black ju">www.growxth.com</p>
             </div>
+            <p className="flex pt-5 text-sm text-black ju">
+              14 Sir Baron Jayathilake Mawatha, Colombo 00100
+            </p>
           </div>
         </div>
       </div>
